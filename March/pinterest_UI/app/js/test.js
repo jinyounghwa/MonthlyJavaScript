b8ccca895ebc128b2d578f2236deb40230f3e1df
jinyounghwa <timotolkie@gmail.com>
2017-03-20 20:26:13 +0900

    i don'know

--- March/pinterest_UI/app/js/test.js
+++ March/pinterest_UI/app/js/test.js
@@ -2,26 +2,29 @@ var oReq = new XMLHttpRequest();
 oReq.addEventListener('load',function(evt){
   var data = JSON.parse(this.responseText);
   var div = document.getElementsByClassName('content')[0];
-  var html = data.map(function(v,i) {
 
-    return "<div><img src=\"" + v.path +"\"></div>";
-}).join('');
+  var html = data.map(function(item,idx) {
+        var prevIdx = idx - 1;
+        var idx = idx;
+        var wr = item.height - item.width; // 이건되는데
+        var wr2 = item.height[prevIdx] - item.height; // 이건 왜 NaN인겨..ㅠㅠ 
+        var height = item.height;
+        var width = item.width;
+        console.log(wr);
+      return {
+        wheight : wr,
+        path : item.path
+      };
+      }).map(function(item){
+
+        return "<div><img src=\"" + item.path +"\"></div>";
+    }).join('');
+
 div.innerHTML = html;
 });
 
-// var oReq = new XMLHttpRequest();
-// oReq.addEventListener('load',function(evt){
-//   var data = JSON.parse(this.responseText);
-//       function getFields(list, field, otherwise) {
-//         return list.map(function(item) {
-//
-//         return typeof item === 'object' && field in item ? item[field] : otherwise;
-//     }).map(function(width, height, path){
-//
-// },[]);
-//
-// }
-//
-// });
+
+
+
 oReq.open("GET", "../apis/list.json");
 oReq.send();
