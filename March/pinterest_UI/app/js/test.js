7b0c560526af514eb362af2952c913aa1b11ca94
jinyounghwa <timotolkie@gmail.com>
2017-03-28 16:44:50 +0900

    i dont know inline css value

--- March/pinterest_UI/app/js/test.js
+++ March/pinterest_UI/app/js/test.js
@@ -1,29 +1,135 @@
+
 var oReq = new XMLHttpRequest();
 oReq.addEventListener('load',function(evt){
   var data = JSON.parse(this.responseText);
-  var divList = document.getElementsByClassName(".content");
+  for (var key in data) {
 
 
-  var listNode = data.reduce(function(prev,curr,index,array){
-        var lastInfo = array[array.length - 1];
-        var prevwidth = prev.x;
-        var preveight = prev.y;
-        if(!lastInfo) { // 이동이 없는 초기 1번째 이미지
-        prevheight = 0;
-        prevwidth = 0;
-        } else { // 나머지 4개 이미지 x값은 marginLeft, y값은   marginTop
-        curr.height = curr.y;
-        curr.width = curr.x;
-      }
-        return curr;
-},[])
-  console.log(listNode);
+
+      // document.querySelector(".demo").insertAdjacentHTML("beforebegin","<div><img src=\"" + data[key].path +"\"></div>");
+      //console.log(data[key].width,data[key].height);
+
+      // setposition = Array.prototype.slice.call([setposition]);
+      //var arr = [];
+      //var marge = Array.prototype.push.apply(setposition);
+      //console.log(arr); // 배열이 합쳐지지 않음
+      //var arr3 = [...setposition];
+      //console.log(arr3); // 배열이 합쳐지지 않음
+      //var mgarr = arr.concat(setposition.length);
+      // console.log(mgarr);
+      // var pic = document.querySelectorAll(".content div");
+      // pic = Array.prototype.slice.call(pic);
+      // var top =  (data[key].height) * -1;
+      // for (var i = 0; i < pic.length; i++) {
+      //     console.log(top);
+      //     pic[i].style.marginTop = top+"px";
+      // }
+
+  }
+  // var pic = document.querySelectorAll(".content div");
+  // pic = Array.prototype.slice.call(pic);
+  //console.log(pic);
+  // var newArr = data
+  //     .map(function (el,idx){
+  //         var top =  (el.height) * -1
+          //하드코딩 : 마이너스값은 받아오는데 이걸 마진에 인라인으로 넣을 수가 없음
+          //console.log(top);
+          // pic[1].style.marginTop = "-164px";
+         // pic[1].style.marginLeft = "240px";
+          // pic[2].style.marginTop = "-240px";
+          // pic[3].style.marginTop = "-404px";
+          // pic[3].style.marginLeft = "240px";
+          // pic[4].style.marginTop = "-240px";
+        // pic[4].style.marginLeft = "240px";
+        //   return el;
+        // })
+
+
 });
-// 막힌거 reduce 반환을 다시 accumulator로
-// 그걸 가지고 map 돌려서 li 엘레멘트의 배열로 만들고
-// 마지막으로 forEach 써서 appedChild 
 
+// var oReq = new XMLHttpRequest();
+// oReq.addEventListener('load',function(evt){
+//   var data = JSON.parse(this.responseText);
+//   var div = document.getElementsByClassName('content')[0];
+//   var html = data.map(function(item,idx) {
+//         // idx가 0일 때 아무런 이동이 없기 때문에 조건 추가
+//       var top = item.height;
+//       var left = item.width;
+//       if (left !== 0 ) {
+//         console.log('ok');
+//       } else {
+//
+//       }
+//           return item;
+//       }).map(function(item){
+//         return "<div><img src=\"" + item.path +"\"></div>";
+//     }).join('');
+//
+// div.innerHTML = html;
+// });
+//
 
 
 oReq.open("GET", "../apis/list.json");
 oReq.send();
+
+
+// var oReq = new XMLHttpRequest();
+// oReq.addEventListener('load',function(evt){
+//   var data = JSON.parse(this.responseText);
+//   var div = document.getElementsByClassName('content')[0];
+//
+//   var html = data.map(function(item,idx) {
+//
+//         // Array.prototype.map.call(datafunction(){})
+//         var prevIdx = idx - 1;
+//         var idx = idx;
+//         var wr = item.height - item.width;
+//         var height = item.height;
+//         var width = item.width;
+//         console.log(wr);
+//       return {
+//         wheight : wr,
+//         path : item.path
+//       };
+//       }).map(function(item){
+//
+//         return "<div><img src=\"" + item.path +"\"></div>";
+//     }).join('');
+//
+// div.innerHTML = html;
+// });
+//
+//
+//
+//
+// oReq.open("GET", "../apis/list.json");
+// oReq.send();
+
+
+//
+// var oReq = new XMLHttpRequest();
+// oReq.addEventListener('load',function(evt){
+//   var data = JSON.parse(this.responseText);
+//   var div = document.getElementsByClassName('content')[0];
+//
+//   var html = data.map(function(item,idx) {
+//
+//         // Array.prototype.map.call(data,function(){})
+      // var arrheight = [Object.values(item)[1]];
+      // var arr = [];
+      // for(var i =0; i < arrheight.length; i++){
+      //     arr = arrheight[i];
+      // }
+      // return arr;
+      // })
+//
+// });
+//
+// });
+//
+//
+//
+//
+// oReq.open("GET", "../apis/list.json");
+// oReq.send();
---
+++ March/pinterest_UI/app/js/test1.js
@@ -0,0 +1,40 @@
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener('load',function(evt){
+  var data = JSON.parse(this.responseText);
+  var div = document.getElementsByClassName('content')[0];
+  var pic = document.querySelectorAll(".content div");
+  pic = Array.prototype.slice.call(pic);
+  var arrayNode = [];
+  var listNode = data.reduce(function(prev,curr,index,array){
+        var lastInfo = array[array.length - 1];
+        var prevwidth = prev.x;
+        var preveight = prev.y;
+        if(!lastInfo) { // 이동이 없는 초기 1번째 이미지
+        prevheight = 0;
+        prevwidth = 0;
+        } else { // 나머지 4개 이미지 x값은 marginLeft, y값은marginTop
+        curr.height = curr.y;
+        curr.width = curr.x;
+      }
+
+        return arrayNode.push(curr);
+},[])
+  var picNode = arrayNode.map(function(item){
+
+    return "<div><img src=\"" + item.path +"\"></div>";
+}).join('');
+  var styleNode = arrayNode.map(function(item,idx){
+    console.log(idx);
+     
+
+    return item;
+  });
+div.innerHTML = picNode;
+});
+
+
+
+
+oReq.open("GET", "../apis/list.json");
+oReq.send();
